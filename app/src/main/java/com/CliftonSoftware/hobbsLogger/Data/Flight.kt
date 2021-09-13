3e212d0b2150af31cd29b2509a9094c0ba079684
Zac <zac16530@gmail.com>
2021-09-13 02:49:25 -0400

    adde plane data structure

--- app/src/main/java/com/CliftonSoftware/hobbsLogger/Data/Flight.kt
+++ app/src/main/java/com/CliftonSoftware/hobbsLogger/Data/Flight.kt
@@ -1,3 +1,15 @@
 package com.CliftonSoftware.hobbsLogger.Data
 
 data class Flight(val id: String, val flightNumber: Int , val hobbsTime: Double)
+
+/*data class Flight(val id: String,
+                    val flightNumber: Int,
+                    val plane: Plane,
+                    val tachTime: Double,
+                    val tachPictures: Picture[]
+                    val hobbsTime: Double,
+                    val hobbsPictures: Picture[],
+                    createdAt: String)
+
+
+ */
---
+++ app/src/main/java/com/CliftonSoftware/hobbsLogger/Data/Plane.kt
@@ -0,0 +1,9 @@
+package com.CliftonSoftware.hobbsLogger.Data
+
+data class Plane (val id: String,
+                  val name: String,
+                  val type: String,
+                  val model: String,
+                  val hasTach: Boolean,
+                  val defaultPlane: Boolean
+                  )
